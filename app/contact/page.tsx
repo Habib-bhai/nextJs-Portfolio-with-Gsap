09279c4244630b90d7e3589e9d61460325c3b963
Habib-bhai <habibahmed918131@gmail.com>
2024-09-16 18:53:44 +0500

    about, contact and portfolio page done

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,19 +6,37 @@ import Heading from '../components/Heading'
 function contact() {
   return (
     <>
-    <div className='w-screen h-screen bg-gray-900'>
+    <div className='w-screen sm:h-[200vh] h-[105vh] bg-gray-900'>
 
     <LoadingScreen/>
     <Navbar/>
     <Heading heading='Contact Me'/>
+    
+    <div className='w-screen  flex justify-center items-center '>
+    <form>
+        
+        <fieldset >
+          <legend >Personal Info</legend>
+          <label htmlFor="name" >Name:</label>
+          <input type="text" id="name" required />
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" required />
+        </fieldset>
 
-      <div>
-        <form >
+       
+        <fieldset>
+          <legend>Subject</legend>
+          <label htmlFor="subject">Your Subject:</label>
+          <input type="text" id="subject" required />
+          <label htmlFor="contact">Contact:</label>
+          <input type="text" id="contact" required />
           
+        </fieldset>
 
-        </form>
-
+        <button id='formBtn' type="submit">submit</button>
+      </form>
       </div>
+      
     </div>
     </>
   )
